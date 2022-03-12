120f1a499662f41a8b417a86f5a74d5ca997e303
michael <michael.inacio@outlook.com>
2022-03-12 15:14:03 -0300

    Revert "Revert "Tabela de convidados, login , cadastro rotas""
    This reverts commit 1d5cf3c1fe3544501b7332d40d8a273b69b90ab3.

---
+++ invitation/src/pages/Login/index.js
@@ -0,0 +1,116 @@
+import React from 'react';
+
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useNavigate } from 'react-router-dom';
+
+const theme = createTheme();
+
+export default function Login() {
+    let navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        // eslint-disable-next-line no-console
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+        });
+
+        // TODO Validar login e chamar o Navigate
+        navigate('/home');
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Grid container component="login" sx={{ height: '100vh' }}>
+                <CssBaseline />
+                <Grid
+                    item
+                    xs={false}
+                    sm={4}
+                    md={7}
+                    sx={{
+                        backgroundImage: 'url(https://source.unsplash.com/random)',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundColor: (t) => (t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900]),
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                    }}
+                />
+                <Grid
+                    item
+                    xs={12}
+                    sm={8}
+                    md={5}
+                    component={Paper}
+                    elevation={6}
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        flexWrap: 'nowrap',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        alignContent: 'center',
+                    }}
+                    square
+                >
+                    <Box
+                        sx={{
+                            my: 8,
+                            mx: 4,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Avatar sx={{ m: 1, bgcolor: 'secondary.login' }}></Avatar>
+                        <Typography component="h1" variant="h5">
+                            Entrar
+                        </Typography>
+                        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+                            <TextField margin="normal" required fullWidth id="email" label="Email" name="email" autoComplete="email" autoFocus />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Senha"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                            />
+                            <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Lembrar-me" />
+                            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+                                Entrar
+                            </Button>
+                            <Grid container>
+                                <Grid item xs>
+                                    <Link href="/" variant="body2">
+                                        Esqueci minha senha
+                                    </Link>
+                                </Grid>
+                                <Grid item xs>
+                                    <Link href="/Register" variant="body2">
+                                        Não possui uma conta? Cadastre-se
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
+        </ThemeProvider>
+    );
+}
