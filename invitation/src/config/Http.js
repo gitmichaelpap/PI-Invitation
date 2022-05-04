05ace32344bc3ae22be9b1be617d3e04642666ef
Bruno Enes <enes.bruno@gmail.com>
2022-05-04 20:51:53 -0300

    heroku

--- invitation/src/config/Http.js
+++ invitation/src/config/Http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://api-invitation-pi.herokuapp.com",
   headers: {
     "Content-type": "application/json"
   }
